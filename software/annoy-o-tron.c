ac8df9192f6ea2fb4fe60dbe81feaf02528299cf
Tudor Vișan <tudor.visan49@gmail.com>
2016-11-15 19:29:33 +0200

    Added hardware and software.

---
+++ software/annoy-o-tron.c
@@ -0,0 +1,20 @@
+/*
+ * Annoy-o-tron
+ *
+ * The annoying buzzer.
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+    while (1)
+    {
+        /* TODO: be annoying. */
+    }
+
+    return 0;
+}
